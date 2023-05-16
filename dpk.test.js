c31119d3549d7c2cd92248ab6484ed18be31ffa1
Andrew Keats <andrew.keats@git.live>
2023-05-16 12:24:22 +0100

    wip: adding more tests to cover existing behaviour

--- dpk.test.js
+++ dpk.test.js
@@ -2,14 +2,31 @@ const { deterministicPartitionKey } = require("./dpk");
 
 describe("deterministicPartitionKey - creates a consistently predictable key for sharding data", () => {
   describe("When no seed value is provided", () => {
-    let reultingKey;
+    let resultingKey;
 
     beforeAll(() => {
-      reultingKey = deterministicPartitionKey();
+      resultingKey = deterministicPartitionKey();
     })
 
     it("then it returns the string literal '0'", () => {
-      expect(reultingKey).toBe("0");
+      expect(resultingKey).toBe("0");
+    });
+  });
+
+  describe("When a seed value is provided", () => {
+    describe.each`
+      seedValue | seedValueDescription | expected | expectedDescription
+      ${``} | ${`an empty string`} | ${`0`} | ${`the '0' string literal is returned`}
+    `(`that is $seedValueDescription`, ({ seedValue, expected, expectedDescription }) => {
+      let resultingKey;
+
+      beforeAll(() => {
+        resultingKey = deterministicPartitionKey(seedValue);
+      })
+
+      it(`then ${expectedDescription}`, () => {
+        expect(resultingKey).toBe(expected);
+      });
     });
   });
 });
