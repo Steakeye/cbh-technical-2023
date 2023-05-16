f8774a6041bf264901e12c3eed67c322a7aac6f2
Andrew Keats <andrew.keats@git.live>
2023-05-16 11:54:55 +0100

    refactore of test style to keep test block purely used for assertions
    allowing the code execution to occur in the `when` block/scope

--- dpk.test.js
+++ dpk.test.js
@@ -2,9 +2,14 @@ const { deterministicPartitionKey } = require("./dpk");
 
 describe("deterministicPartitionKey - creates a consistently predictable key for sharding data", () => {
   describe("When no seed value is provided", () => {
+    let reultingKey;
+
+    beforeAll(() => {
+      reultingKey = deterministicPartitionKey();
+    })
+
     it("then it returns the string literal '0'", () => {
-      const trivialKey = deterministicPartitionKey();
-      expect(trivialKey).toBe("0");
+      expect(reultingKey).toBe("0");
     });
   });
 });
